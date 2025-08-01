ad39766bfbae2bc255081976644cd98234c30d8f
Elia Grayeb <77541181+elia1359@users.noreply.github.com>
2025-08-01 13:37:03 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,21 +4,20 @@ from qiskit import QuantumCircuit, execute
 from qiskit_aer import Aer
 
 # Bring QuantumCircuit and put it inside "Elia" virable for use
-# Bring Harmade To Make 1 & 0 (At Same time and mommnet) Happend [That's the Goal of Quantum Computers]
+# Bring Hadamard To Make 1 & 0 (At the same time and moment) Happend [That's the Goal of Quantum Computers]
 # Make The 0, 1, 2, 3, 4, 5 on (if the light 0 is on) or off
 # Measure All The QuantumCircuit  
 Elia = QuantumCircuit(6, 6)
 Elia.h(0)
 Elia.cx(0, 5)
-Elia.measure_all()
+Elia.measure_all()  
 
-# We Need an 'Aer' Simulator (Not a Real Quantum Computer). Bring qasm_ simulator
-# We Need an 'execute' so we can Bring 'Elia' and 'Simulator both The measure in shots = 1300
-# We Need "Job" so we so we can get counts inside the result
-simulator = Aer.get_backend('qasm_simulator')
-result = execute(Elia, Simulator, shots=1300). result()
+# We Need an 'Aer' Simulator (Not a Real Quantum Computer). Bring qasm_simulator
+# We Need an 'execute' so we can Bring 'Elia' and 'Simulator' both. The measure in shots = 1300
+# We Need "Job" so we can get counts inside the result
+Simulator = Aer.get_backend('qasm_simulator')
+result = execute(Elia, Simulator, shots=1300).result()
 counts = result.get_counts()
 
 # Give The Real Result on the Screen by print the "counts" virable
 print(counts)
-
